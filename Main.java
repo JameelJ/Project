c1fbe71a7add01646a257162b7325290f6c9baf5
JameelJ <103939482+JameelJ@users.noreply.github.com>
2022-05-04 18:31:03 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 package project;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -27,14 +32,12 @@ public class Main {
 		items.add(inkBottle);
 		items.add(gelpen);
 		Main main = new Main();
+		
 		System.out.println("\t*****Welcome to Stationary*****");
 		main.inputMenu();
 	}
 	public void inputMenu() {
 		System.out.print("Enter Your Name : ");
-//		String namePattern = "[^\\p{P}|^\\d+]+";
-//		true if name contains only alphabets, false - otherwise
-//		boolean result =name.matches(namePattern);
 		customerName = sc.nextLine();
 		System.out.println(":-)Thank You for Entering Your Valuable Name\nThe Available Items Are");
 		initialDisplayItem();	
@@ -116,21 +119,72 @@ public class Main {
 		}else if(continueProcess=='d'||continueProcess=='D'){
 			displayItemsAgain();
 		}else {
-			printBill();
-		}
+			printBill();;
+		}     
 	}
 	public void printBill(){
-		 Iterator<ProductBill> iterate =bill.iterator();
-		 LocalDateTime now = LocalDateTime.now();  
-		 DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");  
-	     String formatDateTime = now.format(format); 
-	     System.out.println("\t\t******Stationary Store********");
-	     System.out.println("Customer Name : "+customerName);
-	     System.out.println("Date & Time:"+formatDateTime);
-	     System.out.println("Productid\t"+"Productname"+"\tQuantity(Q)"+"\tPrice/(Q)"+"\tPrice");
-		 while(iterate.hasNext()) {
+		
+		LocalDateTime now = LocalDateTime.now(); 
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");  
+		String formatDateTime = now.format(format); 
+		System.out.println("\t\t******Stationary Store********");
+	    System.out.println("Customer Name : "+customerName);
+	    System.out.println("Date & Time:"+formatDateTime);
+	    System.out.println("\nProductid\t"+"Productname"+"\tQuantity(Q)"+"\tPrice/(Q)"+"\tPrice");
+	    Iterator<ProductBill> iterate =bill.iterator();
+	    while(iterate.hasNext()) {
 			System.out.println(iterate.next());
-		}System.out.println("\tYour Total Payable Amount is Rupees : "+totalAmount);
-		 System.out.println("\t*****Thank You For Shopping Visit Again:-)*****");
+		}
+	    System.out.println("\tYour Total Payable Amount is Rupees : "+totalAmount);
+		System.out.println("\t*****Thank You For Shopping Visit Again:-)*****");
+		saveFile();
+		System.out.println("To see Customer Details Press Y or Anyother to exit");
+		char ch= sc.next().charAt(0);
+		if(ch=='Y'||ch=='y') {
+			showCustomerFileList();
+		}else {
+			return;
+		}
+	}
+	public void saveFile()
+	{
+		LocalDateTime now = LocalDateTime.now(); 
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");  
+		String formatDateTime = now.format(format); 
+		File file = new File("C:\\Users\\Mohamed Jameel\\Desktop\\Store\\CustomerBill.txt");
+		try {
+			FileWriter wr = new FileWriter(file, true);
+			file.createNewFile();
+//			FileWriter wr = new FileWriter(file);
+			wr.write("\n\t******Stationary Store********"+"\nCustomer Name : "+customerName
+					+ "\nDate & Time : "+formatDateTime);
+			wr.write("\nProductid\t"+"Productname"+"\tQuantity(Q)"+"\tPrice/(Q)"+"\tPrice");
+			Iterator<ProductBill> iterate =bill.iterator();
+			while(iterate.hasNext()) {	
+				 ProductBill a= iterate.next();
+					wr.write("\n\t"+a.getProductId()+"\t" +a.getProductName()+"\t"+a.getQuantity()+"\t\t"+a.getPiecePrice()+"\t\t"+a.getPrice());
+			 }
+			wr.write("\nYour Total Payable Amount is Rupees : "+totalAmount);
+			wr.write("\n\t*****Thank You For Shopping Visit Again:-)*****");
+			wr.flush();
+			wr.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	public void showCustomerFileList() {
+		File file = new File("C:\\Users\\Mohamed Jameel\\Desktop\\Store\\CustomerBill.txt");
+		FileReader rd;
+		try {
+			rd = new FileReader(file);
+			int output = rd.read();
+			while(output!=-1) {
+				System.out.print((char)output);
+				output=rd.read();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
